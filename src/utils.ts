7411d0ed96927cc7f3018a475063402812246a25
Nico Jansen <jansennico@gmail.com>
2017-04-12 21:57:18 +0200

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,24 @@
 import * as fs from 'mz/fs';
+import * as mkdirp from 'mkdirp';
 import * as path from 'path';
 const cmdShim = require('cmd-shim');
+
+const mkdir = (dir: string) => new Promise((res, rej) => {
+    mkdirp(dir, err => {
+        if (err) {
+            rej(err);
+        } else {
+            res();
+        }
+    })
+});
 
 function link(from: string, to: string) {
     to = path.resolve(to)
     const toDir = path.dirname(to)
     const target = path.relative(toDir, from)
     console.log('Symlink:', target, to);
-    return fs.mkdir(path.dirname(to))
+    return mkdir(path.dirname(to))
         .then(_ => fs.symlink(target, to, 'junction'));
 }
 
