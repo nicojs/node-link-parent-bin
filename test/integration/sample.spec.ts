a530c3f00a4285943cb3e18988cfef80475a7db8
Nico Jansen <jansennico@gmail.com>
2020-10-27 20:32:22 +0100

    test(fail): add test for failure propagation (#28)

--- test/integration/sample.spec.ts
+++ test/integration/sample.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import * as childProcess from 'mz/child_process';
+import execa = require('execa');
 import * as path from 'path';
 import * as rimraf from 'rimraf';
 
@@ -17,19 +17,20 @@ const rm = (location: string) =>
 const resolve = (relativePath: string) =>
   path.resolve(__dirname, '../../../sample', relativePath);
 
-const execInSample = (cmd: string, cwd = '') => {
+const execInSample = async (
+  cmd: string,
+  cwd = '',
+): Promise<execa.ExecaReturnValue<string>> => {
   console.log(`exec: ${cmd}`);
-  return childProcess.exec(cmd, { cwd: resolve(cwd) }).then((output) => {
-    const stdout = output[0].toString();
-    const stderr = output[1].toString();
-    if (stdout) {
-      console.log(`stdout: ${stdout}`);
-    }
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-    }
-    return { stdout, stderr };
-  });
+  const output = await execa.command(cmd, { cwd: resolve(cwd) });
+  if (output.stdout) {
+    console.log(`stdout: ${output.stdout}`);
+  }
+  if (output.stderr) {
+    console.error(`stderr: ${output.stderr}`);
+  }
+
+  return output;
 };
 
 const MOCHA_TIMEOUT = 60000;
@@ -74,5 +75,13 @@ describe('Sample project after installing and linking with `link-parent-bin`', f
     )
       .to.eventually.have.property('stdout')
       .and.match(/hello single-binary-dependency/g);
+  });
+
+  it('should proxy the exit code when the process fails', async () => {
+    const result = await expect(
+      execInSample('npm run fail-now', 'packages/child-1'),
+    ).rejected;
+    expect(result.exitCode).eq(3);
+    expect(result.stdout).contains('Fail now!');
   });
 });
